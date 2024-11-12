d5d5524c6a31c13c4605abf1eb863c64b7d56f5b
milamocha <d00253640@student.dkit.ie>
2024-11-12 19:16:28 +0000

    💻 Created pages and template
    (Blacklog item #55435) Created separate app pages for the flask app and created a base template to extend from
    
    Created and styled side navigation bar, dynamic navbar icons, and dynamic page banners

--- app.py
+++ app.py
@@ -3,8 +3,24 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
-def home():
+def index():
     return render_template('index.html')
+
+@app.route('/home')
+def home():
+    return render_template('home.html')
+
+@app.route('/statistics')
+def statistics():
+    return render_template('statistics.html')
+
+@app.route('/information')
+def information():
+    return render_template('information.html')
+
+@app.route('/settings')
+def settings():
+    return render_template('settings.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
