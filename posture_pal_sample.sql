c652c5f45dce9277fb515e45d7123aec9f3cc9aa
Elga J Henry <henryelga2004@gmail.com>
2024-12-05 17:52:46 +0000

    📅 Add SQL queries to create require database and tables Item 57580: Sample SQL queries

--- posture_pal_sample.sql
+++ posture_pal_sample.sql
@@ -1,3 +1,60 @@
+-- Create Database
+DROP DATABASE IF EXISTS posture_pal;
+
+CREATE DATABASE posture_pal;
+
+USE posture_pal;
+
+-- Create required tables
+CREATE TABLE users (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(30),
+    google_client_id VARCHAR(255) UNIQUE NOT NULL,
+    token VARCHAR(255),
+    login INT DEFAULT 0,
+    read_access INT DEFAULT 0,
+    write_access INT DEFAULT 0,
+    email VARCHAR(255)
+);
+
+CREATE TABLE sensor_data (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    user_id INT,
+    temperature FLOAT NOT NULL,
+    temperature_status VARCHAR(10) NOT NULL,
+    humidity FLOAT NOT NULL,
+    humidity_status VARCHAR(10) NOT NULL,
+    pitch FLOAT NOT NULL,
+    gravity_x FLOAT NOT NULL,
+    gravity_y FLOAT NOT NULL,
+    gravity_z FLOAT NOT NULL,
+    timestamp DATETIME DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (user_id) REFERENCES users(id)
+);
+
+CREATE TABLE threshold (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    user_id INT,
+    temp_overheat FLOAT DEFAULT 37.5,
+    temp_cold FLOAT DEFAULT 15,
+    humid_high FLOAT DEFAULT 80,
+    humid_low FLOAT DEFAULT 20,
+    pitch FLOAT DEFAULT 0,
+    gravity_x FLOAT DEFAULT 0,
+    gravity_y FLOAT DEFAULT 0,
+    gravity_z FLOAT DEFAULT 1,
+    FOREIGN KEY (user_id) REFERENCES users(id)
+);
+
+CREATE TABLE power_sessions (
+    id INT AUTO_INCREMENT PRIMARY KEY,  
+    user_id INT NOT NULL,             
+    power_on BOOLEAN NOT NULL,           
+    timestamp DATETIME DEFAULT CURRENT_TIMESTAMP,  
+    FOREIGN KEY (user_id) REFERENCES users(id)  
+);
+
+
 -- Sample User Account
 INSERT INTO users (id, name, google_client_id, token, login, read_access, write_access, email)
 VALUES (1, 'John', '103440899543345016347', NULL, 1, 0, 0, 'johnposturepal@gmail.com');
